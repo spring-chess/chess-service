f090f5c466c0c3dda0556557135e7423a6f6b680
SW33TSTUFF <simaakniyaz@gmail.com>
2025-11-24 01:06:19 +0530

    Add initial project configuration and environment setup

--- src/main/java/com/chess/chess_service/ChessServiceApplication.java
+++ src/main/java/com/chess/chess_service/ChessServiceApplication.java
@@ -1,5 +1,6 @@
 package com.chess.chess_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChessServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(ChessServiceApplication.class, args);
 	}
 
