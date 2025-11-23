c260a6b576640e819395b1a6f5e15e171994b14a
SW33TSTUFF <simaakniyaz@gmail.com>
2025-11-23 23:50:35 +0530

    Initialize project

---
+++ src/main/java/com/chess/chess_service/ChessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chess.chess_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessServiceApplication.class, args);
+	}
+
+}
